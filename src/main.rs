a966361d363f78faaafd51cd5dc1c83deabc0b25
cuckflong <johnathanclong13@gmail.com>
2022-10-25 01:28:31 +0800

    info

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,7 @@ async fn main() {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG")
-                .unwrap_or_else(|_| "hedging_api=debug,tower_http=debug".into()),
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "hedging_api=info,tower_http=info".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
