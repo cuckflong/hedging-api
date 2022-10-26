40f007aaadc3f15e87f153111a14223caf25a9e9
cuckflong <johnathanclong13@gmail.com>
2022-10-26 19:18:28 +0800

    cors

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ async fn main() {
 
     let cors = CorsLayer::new()
         .allow_methods(vec![Method::GET, Method::POST, Method::OPTIONS])
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin("http://localhost:3000*".parse::<HeaderValue>().unwrap())
         .allow_credentials(true);
 
     let app = Router::new()
