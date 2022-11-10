9401f102d9e14766387c80c8da2fadcbd937b0bb
cuckflong <johnathanclong13@gmail.com>
2022-11-11 03:14:20 +0800

    temp: remove extreme values

--- src/db_query/derived.rs
+++ src/db_query/derived.rs
@@ -86,6 +86,10 @@ async fn get_derived_history_json(
             None => (),
         }
 
+        if data_history[i].abs() > 100.0 {
+            continue;
+        }
+
         time_history_new.push(time_history[i]);
         data_history_new.push(data_history[i]);
     }
